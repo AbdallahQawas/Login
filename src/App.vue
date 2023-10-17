68b19cccdd9578a174dc96b546b726b6fe36f6a6
abdallah-nazzal <abdallahqawas@gmail.com>
2023-10-17 20:27:07 +0300

    add template component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import pageMenu from './components/page-menu.vue'
-import pageHeader from './components/page-header.vue'
-import { ref } from 'vue'
-
-let pageLabel = ref('')
-function pageClicked(label: string) {
-  pageLabel.value = label
-}
+import pageTemplate from './components/page-template.vue'
 </script>
 
 <template>
-  <div class="flex flex-row">
-    <div>
-      <pageMenu @on-page-clicked="pageClicked"></pageMenu>
-    </div>
-    <div class="w-full">
-      <div>
-        <pageHeader :page-title="pageLabel" tenant-name="Abdallah"></pageHeader>
-      </div>
-      <div class="bg-gray-200 h-full">test2</div>
-    </div>
+  <div>
+    <pageTemplate />
   </div>
   <router-view />
 </template>
---
+++ src/components/page-template.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-row">
+    <div>
+      <pageMenu @on-page-clicked="pageClicked"></pageMenu>
+    </div>
+    <div class="w-full">
+      <div>
+        <pageHeader :page-title="pageLabel" tenant-name="Abdallah"></pageHeader>
+      </div>
+      <div class="bg-gray-200 h-full">test2</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import pageMenu from './page-menu.vue'
+import pageHeader from './page-header.vue'
+
+import { ref } from 'vue'
+
+let pageLabel = ref('')
+function pageClicked(label: string) {
+  pageLabel.value = label
+}
+</script>
